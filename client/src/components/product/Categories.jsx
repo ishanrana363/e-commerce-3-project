95be6bd5d43ec0e84a55b3351022a9223aee0412
ishanrana363 <ishanrana363@gmail.com>
2024-01-10 16:05:02 +0600

    category api integration done

--- client/src/components/product/Categories.jsx
+++ client/src/components/product/Categories.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import productStore from "../../store/ProductStore.js";
+import CategoriesSkeleton from "../../skeleton/Categories-skeleton.jsx";
 const Categories = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const {categoryList} = productStore();
+    if (categoryList===null){
+        return <CategoriesSkeleton/>
+    }else {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+
 };
 
 export default Categories;
