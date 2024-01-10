7a36706b7976f06dc20404b5107f4b4378584750
ishanrana363 <ishanrana363@gmail.com>
2024-01-10 13:49:19 +0600

    slider skeleton and feature skeleton done

--- client/src/components/layout/AppNavBar.jsx
+++ client/src/components/layout/AppNavBar.jsx
@@ -1,6 +1,8 @@
 import {Link} from "react-router-dom";
 import logo from "../../assets/images/plainb-logo.svg"
 import productStore from "../../store/ProductStore.js";
+import {Form} from "react-bootstrap";
+
 const AppNavBar = () => {
     const {ByKeyWord,ByKeyWordRequest}=productStore();
     return (
@@ -36,43 +38,82 @@ const AppNavBar = () => {
                 </div>
             </div>
         </div>
-        <nav className="navbar  sticky-top bg-white navbar-expand-lg navbar-light py-3">
+
+            <nav className="navbar  sticky-top bg-white navbar-expand-lg navbar-light py-3">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
                         <img className="img-fluid" src={logo} alt="" width="96px" />
                     </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav06" aria-controls="nav06" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="nav06">
-                        <ul className="navbar-nav mt-3 mt-lg-0 mb-3 mb-lg-0 ms-lg-3">
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <span className="nav-item me-4">
                                 <Link className="nav-link" to="/">Home</Link>
                             </span>
                         </ul>
-                    </div>
-                    <div className=" d-lg-flex">
-                        <div className="input-group">
-                            <input  onChange={(e)=>ByKeyWordRequest(e.target.value)} className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                            <Link to={ByKeyWord.length>0?`/by-keyword/${ByKeyWord}`:`/`} className="btn btn-outline-dark" type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{ width: 24, height: 24 }}>
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                </svg>
+                        <div className=" d-lg-flex">
+                            <div className="input-group">
+                                <input  onChange={(e)=>ByKeyWordRequest(e.target.value)} className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                                <Link to={ByKeyWord.length>0?`/by-keyword/${ByKeyWord}`:`/`} className="btn btn-outline-dark" type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{ width: 24, height: 24 }}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                    </svg>
+                                </Link>
+                            </div>
+                                <Link to="/cart" type="button" className="btn ms-2 btn-light position-relative">
+                                        <i className="bi text-dark bi-bag"></i>
+                                </Link>
+                            <Link to="/wish" type="button" className="btn ms-2 btn-light d-flex">
+                                <i className="bi text-dark bi-heart"></i>
                             </Link>
+                            <Link type="button" className="btn ms-3 btn-success d-flex" to="/profile">Profile</Link>
+                            <Link type="button" className="btn ms-3 btn-success d-flex" to="/profile">Logout</Link>
                         </div>
-                        <Link to="/cart" type="button" className="btn ms-2 btn-light position-relative">
-                            <i className="bi text-dark bi-bag"></i>
-                        </Link>
-                        <Link to="/wish" type="button" className="btn ms-2 btn-light d-flex">
-                            <i className="bi text-dark bi-heart"></i>
-                        </Link>
-                        <Link type="button" className="btn ms-3 btn-success d-flex" to="/profile">Profile</Link>
-                        <Link type="button" className="btn ms-3 btn-success d-flex" to="/profile">Logout</Link>
                     </div>
                 </div>
             </nav>
+
         </>
     );
 };
 
 export default AppNavBar;
+
+
+
+// <nav className="navbar  sticky-top bg-white navbar-expand-lg navbar-light py-3">
+//     <div className="container">*/}
+//        <Link className="navbar-brand" to="/">*/}
+//             <img className="img-fluid" src={logo} alt="" width="96px" />*/}
+//         </Link>
+//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav06" aria-controls="nav06" aria-expanded="false" aria-label="Toggle navigation">*/}
+//             <span className="navbar-toggler-icon"></span>*/}
+//         </button>*/}
+//         <div className="collapse navbar-collapse" id="nav06">*/}
+//             <ul className="navbar-nav mt-3 mt-lg-0 mb-3 mb-lg-0 ms-lg-3">*/}
+//                <span className="nav-item me-4">*/}
+//                <Link className="nav-link" to="/">Home</Link>*/}
+//                </span>*/}
+//         </div>*/}
+//         <div className=" d-lg-flex">*/}
+//            <div className="input-group">*/}
+//                 <input  onChange={(e)=>ByKeyWordRequest(e.target.value)} className="form-control" type="search" placeholder="Search" aria-label="Search" />*/}
+//                 <Link to={ByKeyWord.length>0?`/by-keyword/${ByKeyWord}`:`/`} className="btn btn-outline-dark" type="submit">*/}
+//                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{ width: 24, height: 24 }}>*/}
+//                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />*/}
+//                   </svg>*/}
+//                </Link>*/}
+//             </div>*/}
+//             <Link to="/cart" type="button" className="btn ms-2 btn-light position-relative">*/}
+//                 <i className="bi text-dark bi-bag"></i>*/}
+//            </Link>*/}
+//            <Link to="/wish" type="button" className="btn ms-2 btn-light d-flex">*/}
+//               <i className="bi text-dark bi-heart"></i>*/}
+//             </Link>*/}
+//            <Link type="button" className="btn ms-3 btn-success d-flex" to="/profile">Profile</Link>*/}
+//             <Link type="button" className="btn ms-3 btn-success d-flex" to="/profile">Logout</Link>*/}
+//         </div>*/}
+//    </div>*/}
+// </nav>
--- client/src/pages/Home-page.jsx
+++ client/src/pages/Home-page.jsx
@@ -1,11 +1,11 @@
 import Layout from "../components/layout/Layout.jsx";
+import FeaturesSkeleton from "../skeleton/FeaturesSkeleton.jsx";
 
 const HomePage = () => {
     return (
         <div>
             <Layout>
-
-                
+                <FeaturesSkeleton/>
             </Layout>
         </div>
     );
